19ca6c6a734204fd35085586ce7dfbd1434d6e80
Tadahisa MOTOOKA <motooka@jotojoho.co.jp>
2021-09-15 23:16:13 +0900

    Initial Commit

---
+++ jinsei/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  jinsei
+//
+//  Created by T. MOTOOKA on 2021/09/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ jinsei/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  jinsei
+//
+//  Created by T. MOTOOKA on 2021/09/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
